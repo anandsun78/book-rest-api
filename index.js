1936060bd833667d87c6e6162a7a3bcfbcdad0f4
anandsun78 <anandsun78@gmail.com>
2020-12-22 18:41:51 -0700

    Implemented all REST methods

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
+const db = mongoose.connect("mongodb://localhost/bookAPI", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 const app = express();
+const Book = require("./models/bookModel");
+const bookRouter = require("./routes/bookRouter")(Book);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use("/api", bookRouter);
 
 const port = process.env.PORT || 3000;
 
